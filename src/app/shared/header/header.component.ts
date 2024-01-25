46f0968cfc1aee3f1c0d79eca7c045e420925241
Yorman Valencia <yormanvale@outlook.com>
2024-01-25 00:42:27 -0500

    footer y header responsive completos

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent{
+  public active: boolean = false;
+  public active__menu : boolean = false;
 
+  setActive(): void {
+    this.active = !this.active;
+  }
+
+  setActiveMenu(): void {
+    this.active__menu = !this.active__menu;
+  }
 }
